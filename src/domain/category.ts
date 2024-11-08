b389e25dec833576351245b7c0ff293b5de04bb4
Rodrigo Martins <39654971+drigomartins@users.noreply.github.com>
2024-11-08 10:09:01 -0300

    feat: upload initial files

---
+++ src/domain/category.ts
@@ -0,0 +1,18 @@
+export type Category =
+  | 'Kim-Min-gyu'
+  | 'Joshua Hong'
+  | 'Jeon Wonwoo'
+  | 'The8'
+  | 'Yoon jeong han'
+  | 'DK'
+  | 'Wen Junhui'
+  | 'Woozi'
+  | 'Choi Seung-cheol'
+  | 'Seungkwan'
+  | 'Vernon'
+  | 'Hoshi'
+  | 'Dino'
+  | 'Seventeen'
+
+
+
---
+++ src/infra/wallpaper-list.ts
@@ -0,0 +1,29 @@
+import { WallpaperItem } from '@/domain';
+
+import { kimMinGyu } from './kim-Min-gyu';
+import { joshuaHong } from './joshua-hong';
+import { jeonWonwoo } from './jeon-wonwoo';
+import { the8 } from './the8';
+import { yoonJeongHan } from './yoon-jeong-han';
+import { wenJunhui } from './wen-junhui';
+import { dk } from './dk';
+import { woozi } from './woozi';
+import { choiSeungCheolk } from './choi-seung-cheol';
+import { seungkwan } from './seungkwan';
+import { hoshi } from './hoshi';
+import { seventeen } from './seventeen';
+
+export const wallpaperList: WallpaperItem[] = [
+  ...kimMinGyu,
+  ...joshuaHong,
+  ...jeonWonwoo,
+  ...the8,
+  ...yoonJeongHan,
+  ...wenJunhui,
+  ...dk,
+  ...woozi,
+  ...choiSeungCheolk,
+  ...seungkwan,
+  ...hoshi,
+  ...seventeen,
+];
